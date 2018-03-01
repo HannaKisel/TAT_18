39cbb997351dd56b8e4d8ca8fb65ca7b504b1600
Anna Moroz <amoroz408@gmail.com>
2018-03-01 22:00:01 +0600

    made the first task

---
+++ EqualSymbols/EqualSymbols/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EqualSymbols
+{
+  class EntryPoint
+  {
+    static void Main()
+    {
+      Console.Write("Enter character sequence: ");
+      try
+      {
+        string characterSequence = Console.ReadLine();
+        WorkerWithString workerWithString = new WorkerWithString();
+        int maxRepetitions = workerWithString.DetermineMaxOfRepetitions(characterSequence);
+        Console.WriteLine("Maximum symbol repetition = " + maxRepetitions);
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("FormatException");
+      }
+    }
+  }
+}
---
+++ EqualSymbols/EqualSymbols/WorkerWithString.cs
@@ -0,0 +1,40 @@
+﻿namespace EqualSymbols
+{
+  /// <summary>
+  /// This class contains a method that works with a string
+  /// </summary>
+  class WorkerWithString
+  {
+    /// <summary>
+    /// This method counts the maximum number of identical consecutive characters in a string
+    /// </summary>
+    /// <param name="characterSequence">original character sequence</param>
+    /// <returns> The maximum number of identical consecutive characters in a string </returns>
+    public int DetermineMaxOfRepetitions(string characterSequence)
+    {
+      int maxRepetitions = 0;
+      for (int i = 0; i < (characterSequence.Length - 1); i++)
+      {
+        int counter = 0;
+        char firstSymbol = characterSequence[i];
+        for (int j = i + 1; j < characterSequence.Length; j++)
+        {
+          char nextSymbol = characterSequence[j];
+          if (firstSymbol == nextSymbol)
+          {
+            counter++;
+          }
+          else
+          {
+            break;
+          }
+        }
+        if (maxRepetitions < counter)
+        {
+          maxRepetitions = counter;
+        }
+      }
+      return maxRepetitions + 1;
+    }
+  }
+}
