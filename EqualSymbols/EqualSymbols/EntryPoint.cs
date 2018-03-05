cd10bdd770e10daf2265c9fe400ba57c72d1eaf5
Anna Moroz <amoroz408@gmail.com>
2018-03-05 23:50:18 +0300

    Add dev-1

---
+++ EqualSymbols/EqualSymbols/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EqualSymbols
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        Sequence sequence = new Sequence(args[0]);
+        int maxRepetitions = sequence.DetermineMaxOfRepetitions();
+        Console.WriteLine("Maximum symbol repetition = " + maxRepetitions);
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("FormatException");
+      }
+    }
+  }
+}
