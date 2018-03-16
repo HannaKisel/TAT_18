823f26de7be01f2aeb201e8ff028f8a5e93b2de8
Anna Moroz <amoroz408@gmail.com>
2018-03-16 17:35:17 +0300

    Added UnitTests for task DEV-3

---
+++ DEV-3/NumberConverter.Tests/NumberHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NumberConverter.Tests
+{
+  [TestFixture]
+  public class NumberHandlerTests
+  {
+    [Test, TestCase(10, 2, "1010")]
+    [TestCase(10, 20, "A")]
+    [TestCase(0, 20, "0")]
+    [TestCase(49, 10, "49")]
+    [TestCase(int.MaxValue, 16, "7FFFFFFF")]
+    public void ConvertNumber_TrueValueExpected(int number, int systemBase, string expected)
+    {
+      Assert.AreEqual(expected, new NumberHandler(number, systemBase).ConvertNumber());
+    }
+
+    [Test, TestCase(-10, 20)]
+    [TestCase(10, -1)]
+    [TestCase(10, 21)]
+    public void ConvertNegativeNumber_FalseValueExpected(int number, int systemBase)
+    {
+      Assert.Throws<ArgumentException>(() => new NumberHandler(number, systemBase).ConvertNumber());
+    }
+  }
+}
---
+++ DEV-3/NumberConverter.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NumberConverter.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NumberConverter.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("828723bf-45fc-46a1-8ca4-abadb383e137")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
