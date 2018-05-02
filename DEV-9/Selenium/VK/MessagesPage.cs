f69f472b9481d13af89132d9247e990cb639a472
Anna Moroz <amoroz408@gmail.com>
2018-05-02 16:08:49 +0300

    Added task DEV-9

---
+++ DEV-9/Selenium/VK/MessagesPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Linq;
+
+namespace VK
+{
+  public class MessagesPage
+  {
+    private IWebDriver webDriver;
+    private readonly By Message = By.XPath("//span[contains(text(),'Сообщения')]");
+    private readonly By Dialogs = By.XPath("//div[@class='nim-dialog--cw']");
+    private readonly By Messages = By.XPath("//div[@class='im-mess--text wall_module _im_log_body']");
+    private readonly By DialogsArea = By.XPath("//ul[@id='im_dialogs']");
+    private readonly By Chat = By.XPath("//div[@class='im-page--chat-body']");
+
+    public MessagesPage(IWebDriver webdriver)
+    {
+      this.webDriver = webdriver;
+    }
+
+    public MessagesPage ReadFirstMessage()
+    {
+      WaitForElement(DialogsArea);
+      webDriver.FindElements(Dialogs).First().Click();
+      WaitForElement(Chat);
+      string message = webDriver.FindElements(Messages).Last().Text;
+      Console.WriteLine(message);
+      return this;
+    }
+
+    private void WaitForElement(By locator)
+    {
+      new WebDriverWait(webDriver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementIsVisible(locator));
+    }
+  }
+}
