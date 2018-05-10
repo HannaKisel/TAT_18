2483ef25d72531834704969c77dc136fd0fbbf1f
Anna Moroz <amoroz408@gmail.com>
2018-05-10 16:39:40 +0300

    Added task_DEV-10

---
+++ DEV-10/CarSercher/CarSercher/EntryPoint.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+
+namespace CarSercher
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        IWebDriver driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+        driver.Navigate().GoToUrl("https://av.by/");
+        Console.Clear();
+        HomePage homePage = new HomePage(driver);
+        homePage.ClosePopUpIfExist().ClickOnShowAllMarksButton();
+        MarkPage markPage = homePage.ClickOnCarMark(args[0]);
+        Dictionary<string, string> modelsList = markPage.CreateDictionaryCar();
+        CarDictionaryHandler carDictionary = new CarDictionaryHandler();
+        List<KeyValuePair<string, string>> sortedCarsByCount = carDictionary.SortCarDictionaryInDescendingOrder(modelsList);
+        carDictionary.DisplayListOfModelsInConsole(sortedCarsByCount);
+      }
+      catch (Exception exception)
+      {
+        Console.WriteLine(exception.Message);
+      }
+    }
+  }
+}
