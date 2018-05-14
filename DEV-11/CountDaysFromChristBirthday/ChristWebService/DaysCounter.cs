7a1a8222c741f9bfb42f6f02ca1ecb1076e2e657
Anna Moroz <amoroz408@gmail.com>
2018-05-14 15:31:43 +0300

    Started task DEV-11

---
+++ DEV-11/CountDaysFromChristBirthday/ChristWebService/DaysCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Services;
+
+namespace ChristWebService
+{
+  /// <summary>
+  /// this class contains a method that counts the number of days from 
+  /// the birth of Christ until the entered date
+  /// </summary>
+  [WebService(Description = "This web-service counts days from Christ birthday to your entered date", Namespace = tempuriNamespace)]
+  public class DaysCounter
+  {
+    const string tempuriNamespace = "http://tempuri.org/";
+
+    /// <summary>
+    /// The method that counts the number of days from the birth of Christ until the date you entered
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    [WebMethod]
+    public int CountDays(DateTime date)
+    {
+      DateTime dateOfChristBirthday = new DateTime(0001, 1, 1);
+      return (int)(date - dateOfChristBirthday).TotalDays;
+    }
+  }
+}
---
+++ DEV-11/CountDaysFromChristBirthday/CountDaysFromChristBirthday/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CountDaysFromChristBirthday
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        Console.WriteLine("Enter day in format dd.mm.year");
+        DateTime date = DateTime.Parse(Console.ReadLine());
+        ChristService.DaysCounter dateCounter = new ChristService.DaysCounter();
+        Console.WriteLine(dateCounter.CountDays(date));
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
