ac2edb25315f50f005af501fa222c7b186b14b20
Anna Moroz <amoroz408@gmail.com>
2018-05-21 19:49:19 +0300

    Added the first task

---
+++ Test/HelloWorld/HelloWorld/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, world!\nAnd hi again!");
+      Random random = new Random();
+      int exclamationNumber = random.Next(5, 50);
+      for (int i = 0; i < exclamationNumber; i++)
+      {
+        Console.Write("!");
+      }
+    }
+  }
+}
